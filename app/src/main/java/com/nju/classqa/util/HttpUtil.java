68d674d95d87b18a55a0dfff40ed617cdee95da3
171250647 <171250647@smail.nju.edu.cn>
2019-05-26 21:08:06 +0800

    Merge branch 'master' of https://github.com/cxkrap/classQA
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/nju/classqa/util/HttpUtil.java
+++ app/src/main/java/com/nju/classqa/util/HttpUtil.java
@@ -51,6 +51,5 @@ public class HttpUtil {
         OkHttpClient client=new OkHttpClient();
         Request request=new Request.Builder().url(address).build();
         client.newCall(request).enqueue(callback);
-
     }
 }
